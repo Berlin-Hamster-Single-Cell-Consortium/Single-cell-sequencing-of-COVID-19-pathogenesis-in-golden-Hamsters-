763f81fc0ec6278f22909f8090a46caf0396706b
emanuelwyler <emanuel.wyler@mdc-berlin.de>
2021-01-25 16:07:55 +0100

    Create lung_scRNAseq_integrate.R

---
+++ lung_scRNAseq_integrate.R
@@ -0,0 +1,50 @@
+library(Seurat)
+library(dplyr)
+library(cowplot)
+library(patchwork)
+library(ggplot2)
+library(ggpubr)
+#------------------------------------------------------------------------------------------
+
+#### PREPROCESS DATA ####
+
+
+##Read in data from CellRanger output
+
+hamster_all <- readRDS("./seu_lung_combined_mtfilt.rds")
+
+
+DefaultAssay(hamster_all) <- "RNA"
+
+
+## integrate by hamster to remove batch effects
+
+hamster.list <- SplitObject(hamster_all, split.by = "orig.ident")
+for (i in 1:length(hamster.list)){
+  hamster.list[[i]] <- SCTransform(hamster.list[[i]], verbose =T)
+}
+
+hamster.features <- SelectIntegrationFeatures(object.list = hamster.list, nfeatures = 3000)
+hamster.list <- PrepSCTIntegration(object.list = hamster.list, anchor.features = hamster.features, 
+                                   verbose = T)
+
+hamster.anchors <- FindIntegrationAnchors(object.list = hamster.list, normalization.method = "SCT", 
+                                          anchor.features = hamster.features, verbose = T)
+hamster.integrated <- IntegrateData(anchorset = hamster.anchors, normalization.method = "SCT", 
+                                    verbose = T)
+
+## run dimensional reductions
+#   PCA
+hamster.integrated<- RunPCA(hamster.integrated, verbose = FALSE)
+#   UMAP
+hamster.integrated<- RunUMAP(hamster.integrated, dims = 1:30, verbose = FALSE)
+
+
+#### CLUSTERING AND ANNOTATION ####
+
+##cluster
+hamster.integrated <- FindNeighbors(hamster.integrated, dims = 1:30)
+hamster.integrated <- FindClusters(hamster.integrated, resolution = 0.5)
+
+
+saveRDS(hamster.integrated, "./seu_lung_combined_mtfilt.integrated.rds")
